4a8acaf7928c728a15135f168fcb9c157904c205
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-04 14:33:32 -0300

    backup aula

---
+++ src/pkg1/main.java
@@ -0,0 +1,14 @@
+package pasta1;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+            Conta contaVinicius =  new Conta();
+            Conta contaJoao = new Conta();
+            contaVinicius.saldo = 100;
+            contaJoao.saldo = 200;
+            System.out.println("Saldo: " + contaVinicius.saldo);
+    }
+    }
+}
