2da6aed261832bd728842b4fd914cda177adad76
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-04 16:01:53 -0300

    getter & setter tempo

---
+++ src/Aula3/Tempo.java
@@ -0,0 +1,29 @@
+package pasta1;
+
+public class Tempo {
+    int horas, minutos, segundos;
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+}
