8614400fb0e5c99dbab2042b9dd183003fcb0dab
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-04 14:40:06 -0300

    Update Conta.java

--- src/pkg1/Conta.java
+++ src/pkg1/Conta.java
@@ -1,8 +1,10 @@
-package pasta1;
-
+package pkg1;
 public class Conta {
     public double saldo;
     public void saque(double valor){
         this.saldo = this.saldo - valor;
+    }
+    public void deposito(double valor){
+        this.saldo = saldo + valor;
     }
 }
