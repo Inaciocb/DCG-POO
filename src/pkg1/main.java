f730f9cdc809d43e6ceff22969429ebee9493571
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-04 15:56:34 -0300

    Update main.java

--- src/pkg1/main.java
+++ src/pkg1/main.java
@@ -1,23 +1,28 @@
 package pasta1;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
             Conta contaInacio = new Conta();
             Conta contaDaniel = new Conta();
+            Retangulo r1 = new Retangulo();
+
+            r1.altura = 3;
+            r1.largura = 2;
+
 
             contaDaniel.setTitular("Daniel Schopf");
+            contaInacio.setTitular("Inacio Cmamargo Buemo");
+
             contaDaniel.setSaldo(300);
             contaInacio.setSaldo(300);
             contaInacio.deposito(20);
-            contaInacio.setTitular("Inacio Cmamargo Buemo");
+
             System.out.println("Saldo: R$ " + contaInacio.getSaldo() + "\nTitular: " + contaInacio.getTitular());
 
 
             contaInacio.transfer(30, contaDaniel);
 
-            System.out.println("Saldo novo do " + contaDaniel.getTitular() + contaDaniel.getSaldo());
-
+            System.out.println("Saldo novo do " + contaDaniel.getTitular() + " " + contaDaniel.getSaldo());
+            System.out.println("Area do r1: " + r1.calcular_area(r1.altura, r1.largura));
     }
 }
