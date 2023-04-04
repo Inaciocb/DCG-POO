dcf38a4bb35215e2df01713ea7dfeb5f35a49c89
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-04 14:45:43 -0300

    Update Conta.java

--- src/pkg1/Conta.java
+++ src/pkg1/Conta.java
@@ -1,8 +1,12 @@
-package pkg1;
+package pasta1;
 public class Conta {
     public double saldo;
     public void saque(double valor){
-        this.saldo = this.saldo - valor;
+        if (valor > this.saldo) {
+            System.out.println("Saldo insuficiente! ");
+        } else{
+            this.saldo = this.saldo - valor;
+        }
     }
     public void deposito(double valor){
         this.saldo = saldo + valor;
