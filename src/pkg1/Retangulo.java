c441789234acaa9c41f840c4aa08ae24c3568237
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-04 15:55:41 -0300

    Create Retangulo.java

---
+++ src/pkg1/Retangulo.java
@@ -0,0 +1,26 @@
+package pasta1;
+
+public class Retangulo {
+    public double altura, largura;
+    public double getLargura() {
+        return largura;
+    }
+    public void setLargura(double largura) {
+        this.largura = largura;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double calcular_area(double x, double y){
+        return x*y;
+    }
+    public double calcular_perimetro(double x, double y){
+        return((x*2)+(y*2));
+    }
+}
