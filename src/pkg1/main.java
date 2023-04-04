fcf82a9bfe4f029407794f51dbb951d7a6cdfb52
Inácio <67322986+Inaciocb@users.noreply.github.com>
2023-04-04 15:25:29 -0300

    Update main.java

--- src/pkg1/main.java
+++ src/pkg1/main.java
@@ -4,9 +4,20 @@ package pasta1;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-            Conta contaInacio =  new Conta();
+            Conta contaInacio = new Conta();
+            Conta contaDaniel = new Conta();
+
+            contaDaniel.setTitular("Daniel Schopf");
+            contaDaniel.setSaldo(300);
             contaInacio.setSaldo(300);
             contaInacio.deposito(20);
-            System.out.println("Saldo: R$ " + contaInacio.getSaldo());
+            contaInacio.setTitular("Inacio Cmamargo Buemo");
+            System.out.println("Saldo: R$ " + contaInacio.getSaldo() + "\nTitular: " + contaInacio.getTitular());
+
+
+            contaInacio.transfer(30, contaDaniel);
+
+            System.out.println("Saldo novo do " + contaDaniel.getTitular() + contaDaniel.getSaldo());
+
     }
 }
